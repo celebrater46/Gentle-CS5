bd72134d506622600a0e612c8226729c4f3406ba
hideru <jota.16.stigma.world@gmail.com>
2021-05-26 16:32:12 +0900

    test

---
+++ Gentle-CS5/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace Gentle_CS5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Console.WriteLine("Test from MainWindow");
+            InitializeComponent();
+        }
+    }
+}
